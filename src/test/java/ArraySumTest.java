40a6b606aff507fee1c55ffe5cd86813a8cf9bae
gasts <gast.steven@gmail.com>
2020-04-10 17:31:27 +0200

    add fukes

---
+++ src/test/java/ArraySumTest.java
@@ -0,0 +1,38 @@
+package test.java;
+
+import main.java.de.hems.ArraySum;
+import org.junit.jupiter.api.*;
+
+
+class ArraySumTest {
+
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("Call BeforeAll");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("Call AfterAll");
+    }
+
+    @Test
+    @DisplayName("create and size test")
+    void t1() {
+      ArraySum a = new ArraySum(10);
+      a.createArray();
+      Assertions.assertEquals(10,a.getSize());
+      Assertions.assertEquals(10,a.getArray().length);
+    }
+
+    @Test
+    @DisplayName("fill array")
+    void t2(){
+        System.out.println("sys: fill array with number 2");
+        ArraySum a = new ArraySum(10);
+        a.createArray();
+        a.fill(10);
+        Assertions.assertEquals(145,a.getSum());
+    }
+}
---
+++ src/test/java/InputOutputTest.java
@@ -0,0 +1,46 @@
+package test.java;
+
+import main.java.de.hems.InputOutput;
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class InputOutputTest {
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    PrintStream originOut = System.out;
+    PrintStream originErr = System.err;
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("sout: Setup streams");
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Test
+    @DisplayName("System.out.print() und System.err.print() test")
+    void test(){
+        InputOutput coClass = new InputOutput();
+        coClass.systemOutPrint("SYS-DEMO");
+        coClass.systemErrPrint("ERR-DEMO");
+        Assertions.assertEquals("SYS-DEMO", outContent.toString());
+        Assertions.assertEquals("ERR-DEMO", errContent.toString());
+    }
+
+    @Test
+    @DisplayName("muss failen")
+    void test2(){
+        Assertions.assertEquals(true, false);
+    }
+
+    @AfterEach
+    void tearDown(){
+        System.setOut(originOut);
+        System.setErr(originErr);
+        System.out.println("sout: unconfig streams done");
+    }
+
+}
